143ca68120bcafc35eb4d03361a0e2e402d53b15
Javier Ospina <jaheosig10@gmail.com>
2023-09-04 21:38:10 -0500

    start crud

---
+++ src/models/users.model.js
@@ -0,0 +1,32 @@
+import {DataTypes} from 'sequelize'
+import db from '../utils/database.js'
+
+const User = db.define('users', {
+  // definirt todos los atributos o columnas de la tabla
+  // id, username, email, password
+  id: {
+    //tipo de dato
+    type: DataTypes.INTEGER,
+    // llave primaria
+    primaryKey: true,
+    //autoincrementable
+    autoIncrement: true,
+  },
+  //username varchar(30) [not null]
+  username: {
+    type: DataTypes.STRING(30),
+    allowNull: false,
+  },
+  // email varchar(50) [not null, unique]
+  email: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+})
+
+export default User
